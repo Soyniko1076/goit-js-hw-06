54cab1ac2227da0520093a70d4056d7b2458f83b
Soyniko1076 <121513340+Soyniko1076@users.noreply.github.com>
2023-04-28 23:01:58 +0300

    123

--- js/task-01.js
+++ js/task-01.js
@@ -2,5 +2,16 @@
 const navItem = document.querySelectorAll(".item")
 console.log("Number of categories:", navItem.length)
 
+console.log(navItem)
 
-// console.log 
+
+
+// const navItemTitle = document.querySelectorAll("h2")
+// console.log(navItemTitle[0].textContent)
+
+// navItemTitle[1].textContent = "Новая категория"
+
+// navItemTitle.forEach(itemTitle => {
+//     console.log(`Category: ${navItemTitle[itemTitle]}, Elements: ${navItemTitle.length}`)
+// });
+
