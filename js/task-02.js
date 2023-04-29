dea9c4703a7883953d971867109b0cef0e9868ed
Soyniko1076 <121513340+Soyniko1076@users.noreply.github.com>
2023-04-29 14:54:17 +0300

    task-02-done

--- js/task-02.js
+++ js/task-02.js
@@ -6,5 +6,33 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
-
 console.log(ingredients)
+
+
+const newArray = ingredients.map((element) => {
+const itemEl = document.createElement("li")
+  itemEl.textContent = element;
+  itemEl.classList.add("item")
+  return itemEl;
+})
+
+console.log(newArray)
+
+
+const listEl = document.querySelector("ul")
+console.log(listEl)
+
+listEl.append(...newArray)
+
+
+
+
+
+
+
+
+
+
+
+
+
