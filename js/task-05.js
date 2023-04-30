1ced874ffa169a818edf9206712f238fafd3d604
Soyniko1076 <121513340+Soyniko1076@users.noreply.github.com>
2023-04-30 20:39:08 +0300

    task-05-done

---
+++ js/task-05.js
@@ -0,0 +1,16 @@
+
+const refs = {
+    input: document.getElementById("name-input"),
+    output: document.getElementById("name-output"),
+}
+
+refs.input.addEventListener("input", inputYourName)
+
+function inputYourName (event) {
+    refs.output.textContent = refs.input.value !== " "
+        ? event.currentTarget.value
+        : 'Anonymous';
+};
+
+
+
